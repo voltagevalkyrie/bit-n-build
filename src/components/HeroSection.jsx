74dd3a546aa378f58c24da3a891c3abb38936f82
AnaghaS-Shastri <149950166+AnaghaS-Shastri@users.noreply.github.com>
2024-02-10 12:34:41 +0530

    hero page edit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,16 +7,17 @@ export function HeroSection() {
         <div className="flex flex-col justify-center px-4 py-12 md:py-16 lg:col-span-7 lg:gap-x-6 lg:px-6 lg:py-24 xl:col-span-6">
           <div className="mt-8 flex text-white max-w-max items-center space-x-2 rounded-full bg-[#0B8185] p-1">
             <div className="rounded-full bg-[#7c2b10d0] p-1 px-2">
-              <p className="text-sm font-medium">Luxurious&apos;</p>
+              <p className="text-sm font-medium">Passionate&apos;</p>
             </div>
-            <p className="text-sm font-medium">Find Your Hotel &rarr;</p>
+            <p className="text-sm font-medium">Find Your Interests &rarr;</p>
           </div>
           <h1 className="mt-8 text-3xl font-bold tracking-tight md:text-4xl lg:text-6xl">
-            Discover amazing Hotels and Enjoy a <span className='text-[#0B8185] capitalize'>memorable stay</span>
+            Discover your favourite Events and <span className='text-[#0B8185] capitalize'>Team up with your matching peers!</span>
           </h1>
           <p className="mt-8 text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur modi blanditiis
-            dolores quasi eaque explicabo!
+          a comprehensive platform that facilitates team formation,
+           event discovery, efficient communication, and seamless coordination 
+            among students, clubs, and event organizers!
           </p>
         </div>
         <div className="relative lg:col-span-5 lg:-mr-8 xl:col-span-6">
