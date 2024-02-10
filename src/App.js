b2f33da30ca0fb96b79772188cda905b929d72df
valkyrie <149293731+voltagevalkyrie@users.noreply.github.com>
2024-02-10 11:35:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./page/Home";
+import CategoryPage from "./page/CategoryPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ErrorPage } from "./page/ErrorPage";
+import SingleHotelView from "./page/SingleHotelView";
+import GlobleCotext from "./contextApi/GlobleContex";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Hello world its mg
-        </p>
-
-      
-
-      </header>
-    </div>
+    <>
+    <GlobleCotext>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Category" element={<CategoryPage/>}/>
+          <Route path={`/SingleHotelView/:id`} element={<SingleHotelView/>} />
+          <Route path="*" element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+      </GlobleCotext>
+    </>
   );
 }
 
