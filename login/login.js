eeebe71c3e687be73dc8fb587e06be17b0ecd68c
valkyrie <149293731+voltagevalkyrie@users.noreply.github.com>
2024-02-11 01:43:39 +0530

    add firebase

---
+++ login/login.js
@@ -0,0 +1,36 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./page/Home";
+import Login from "./login/login";
+import CategoryPage from "./page/CategoryPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ErrorPage } from "./page/ErrorPage";
+import SingleHotelView from "./page/SingleHotelView";
+import GlobleCotext from "./contextApi/GlobleContex";
+import ProtectedRoute from "./pr/pr";
+import Cookie from 'js-cookie';
+
+function App() {
+  const token = Cookie.get('login');
+
+  return (
+    <>
+    <GlobleCotext>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Category" element={<CategoryPage/>}/>
+          {/* <Route path={/SingleHotelView/:id} element={<SingleHotelView/>} /> */}
+          <Route path="*" element={<ErrorPage/>}/>
+          <Route path="/login" element={<Login/>}/> 
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+      </GlobleCotext>
+    </>
+
+  );
+}
+
+export default App;
